4a218abda710299d6072f8c211b806b8976356ca
Daniel Wagner Oliveira de Medeiros <danielwag@gmail.com>
2025-01-13 09:43:59 -0300

    removed yepp

--- DWSIM.Thermodynamics/EditingForms/ExcelInterface/AboutForm.vb
+++ DWSIM.Thermodynamics/EditingForms/ExcelInterface/AboutForm.vb
@@ -23,30 +23,7 @@ Public Class AboutForm
     
         Lblcpuinfo.Text = "Retrieving CPU info..."
 
-        Threading.Tasks.Task.Factory.StartNew(Function()
-                                                  Dim scrh As New System.Management.ManagementObjectSearcher("select * from Win32_Processor")
-                                                  Dim text1 As String = System.Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER")
-                                                  For Each qinfo In scrh.Get()
-                                                      text1 += " / " & qinfo.Properties("Name").Value.ToString
-                                                  Next
-                                                  text1 += " (" & Yeppp.Library.GetProcessABI().Description & ")"
-                                                  Return text1
-                                              End Function).ContinueWith(Sub(t)
-                                                                             Lblcpuinfo.Text = t.Result
-                                                                         End Sub, Threading.Tasks.TaskScheduler.FromCurrentSynchronizationContext)
-
-
         Lblcpusimd.Text = "Querying CPU SIMD capabilities..."
-
-        Threading.Tasks.Task.Factory.StartNew(Function()
-                                                  Dim text1 As String = ""
-                                                  For Each item In Library.GetCpuArchitecture.CpuSimdFeatures
-                                                      text1 += item.ToString & " "
-                                                  Next
-                                                  Return text1
-                                              End Function).ContinueWith(Sub(t)
-                                                                             Lblcpusimd.Text = t.Result
-                                                                         End Sub, Threading.Tasks.TaskScheduler.FromCurrentSynchronizationContext)
 
     End Sub
 
